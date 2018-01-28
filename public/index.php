e857f075b4d028573f175085cfd016e064f221e1
dr.elhabib <motamayze@gmail.com>
2018-01-28 18:25:36 +0100

    add

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Framework\Core\Route;
 
 require_once '../private/vendor/autoload.php';
 
+
 //require_once Config::APP.Config::DR."Framework".Config::DR."Core".Config::DR."AutoLoad.php";
 
 $Route= new Route();
